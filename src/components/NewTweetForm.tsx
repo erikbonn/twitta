0109aaeae3cd72b4ee790ccafd5696b97f94f73e
erikbonn <ebonn89@gmail.com>
2023-07-16 17:29:34 -0600

    feat: add tweet route and mutation

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,7 +1,14 @@
 import { useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import {
+  FormEvent,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { api } from "~/utils/api";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
   if (textArea == null) return;
@@ -21,12 +28,27 @@ function Form() {
 
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaRef.current);
-  }, [inputRef]);
+  }, [inputValue]);
+
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      console.log(newTweet), setInputValue("");
+    },
+  });
 
   if (session.status !== "authenticated") return null;
 
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    createTweet.mutate({ content: inputValue });
+  }
+
   return (
-    <form className="boder-b flex flex-col gap-2 px-4 py-2">
+    <form
+      onSubmit={handleSubmit}
+      className="boder-b flex flex-col gap-2 px-4 py-2"
+    >
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
